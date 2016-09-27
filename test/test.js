0aaecaa29d97a78187873a3c6c21cf51f65fd81a
Thaddee Tyl <thaddee.tyl@gmail.com>
2016-09-27 23:52:50 +0200

    Test guessing labels and a number parameter

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 const fs = require('fs')
 const path = require('path')
+const assert = require('assert')
 const Bot = require('../queread.js')
 let datafile = fs.readFileSync(path.join(__dirname, './dataset'))
 let dataset = Bot.parse(String(datafile))
 let bot = new Bot(dataset)
-let guess = bot.guess('Get me to Madrid.')
-console.log(guess)
+
+let guess = bot.guess('I\'ll take number 2.')
+assert.equal(guess.label, 'select',
+  'Guess a selection')
+assert.equal(guess.parameters.selection, 2,
+  'Guess the number of the selection')
+
+guess = bot.guess('Get me to Madrid.')
+assert.equal(guess.label, 'search',
+  'Guess a search')
